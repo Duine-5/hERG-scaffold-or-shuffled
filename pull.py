19d16d777b82600595153b0bed24d1f81ef7d829
sylphir <mevda0503@gmail.com>
2026-08-18 15:20:13 -0600

    Created a script to pull data for CHEMBL240

---
+++ pull.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import pandas as pd
+from chembl_webresource_client.new_client import new_client
+
+Path("data").mkdir(exist_ok=True)
+
+COLS = [
+    "molecule_chembl_id",
+    "parent_molecule_chembl_id",
+    "canonical_smiles",
+    "standard_type",
+    "standard_relation",
+    "standard_value",
+    "standard_units",
+    #   "pchembl_value", # Not extracted until hypothesis
+    "assay_chembl_id",
+    "assay_type",
+    "data_validity_comment",
+    "potential_duplicate",
+    "target_chembl_id",
+    "document_chembl_id",
+    "document_year",
+]
+
+acts = new_client.activity.filter(target_chembl_id="CHEMBL240", assay_type="B").only(
+    COLS
+)
+
+df = pd.DataFrame(acts)
+df.to_csv("data/CHEMBL240_activities.csv", index=False)
+print(df.shape)
