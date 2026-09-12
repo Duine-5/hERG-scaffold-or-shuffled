0a9f2ef4939dc6eeaa3af9510c292a86bbbb8f9a
sylphir <mevda0503@gmail.com>
2026-09-12 14:24:13 -0600

    linear regression function optimisation and random split function creation

--- linear_regression.py
+++ linear_regression.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 
 
-def linear_regression(Activities, Confidence, Descriptors, Runs):
+def lr_scaffold_split(Activities, Confidence, Descriptors, Runs):
     d = function.filtered_list(Activities, Confidence)
 
     d["scaffolds"] = d["canonical_smiles"].apply(function.scaffold)
@@ -16,12 +16,14 @@ def linear_regression(Activities, Confidence, Descriptors, Runs):
     scaffolds = d["scaffolds"].unique().tolist()
 
     d_group = d.groupby("scaffolds")
+    d_dic = d_group["parent_molecule_chembl_id"].apply(list).to_dict()
 
     mae_list = []
 
     for i in range(Runs):
         print("Run", i)
 
+        np.random.seed(i)  # Set seed for reproducibility
         np.random.shuffle(scaffolds)
 
         # -----------------------------------------------
@@ -30,14 +32,14 @@ def linear_regression(Activities, Confidence, Descriptors, Runs):
         train_ids = []
 
         for scaffold in scaffolds:
-            group = d_group.get_group(scaffold)
-            x = group["parent_molecule_chembl_id"].tolist()
+            x = d_dic[scaffold]
+
             train_ids.extend(x)
             if len(train_ids) / total_count >= 0.8:
                 break
 
-        print(len(train_ids), "train molecules")
-        print(total_count, "total molecules")
+        # print(len(train_ids), "train molecules")
+        # print(total_count, "total molecules")
 
         test_ids = d[~d["parent_molecule_chembl_id"].isin(train_ids)][
             "parent_molecule_chembl_id"
@@ -67,6 +69,56 @@ def linear_regression(Activities, Confidence, Descriptors, Runs):
         predictions = model.predict(test_x)
         mae = mean_absolute_error(p_values_test, predictions)
 
+        mae_list.append(mae)
+
+    return mae_list
+
+
+def lr_rando_split(Activities, Confidence, Descriptors, Runs):
+    d = function.filtered_list(Activities, Confidence)
+
+    d["scaffolds"] = d["canonical_smiles"].apply(function.scaffold)
+
+    d = d.drop_duplicates(subset=["parent_molecule_chembl_id"])
+    d = d.dropna(subset=["scaffolds"])
+
+    mae_list = []
+
+    id_list = d["parent_molecule_chembl_id"].tolist()
+
+    for i in range(Runs):
+        print("Run", i)
+
+        np.random.seed(i)  # Set seed for reproducibility
+        np.random.shuffle(id_list)
+
+        train_ids = id_list[: round(len(id_list) * 0.8)]
+
+        test_ids = id_list[round(len(id_list) * 0.8) :]
+
+        # -----------------------------------------------
+        # linear regression model
+        # -----------------------------------------------
+        # x axis
+        Descriptors_train = Descriptors[
+            Descriptors["parent_molecule_chembl_id"].isin(train_ids)
+        ]
+        train_x = Descriptors_train[["MW", "CLogP", "HBD", "Rotatable_Bonds", "TPSA"]]
+
+        Descriptors_test = Descriptors[
+            Descriptors["parent_molecule_chembl_id"].isin(test_ids)
+        ]
+        test_x = Descriptors_test[["MW", "CLogP", "HBD", "Rotatable_Bonds", "TPSA"]]
+
+        # y axis
+        p_values_train = Descriptors_train["p_values"]
+        p_values_test = Descriptors_test["p_values"]
+
+        model = LinearRegression()
+        model.fit(train_x, p_values_train)
+        predictions = model.predict(test_x)
+
+        mae = mean_absolute_error(p_values_test, predictions)
         mae_list.append(mae)
 
     return mae_list
--- test.py
+++ test.py
@@ -1,8 +1,39 @@
 import linear_regression
+import numpy as np
 import pandas as pd
 
 df_act = pd.read_csv("data/CHEMBL240_activities.csv")
 df_conf = pd.read_csv("data/CHEMBL240_confidence.csv")
 df_desc = pd.read_csv("data/CHEMBL240_descriptors.csv")
 
-print(linear_regression.linear_regression(df_act, df_conf, df_desc, 3))
+runs = 50
+
+print("----------------------------------")
+print("Scaffold split")
+print("----------------------------------")
+scaffold_split = linear_regression.lr_scaffold_split(df_act, df_conf, df_desc, runs)
+print(scaffold_split)
+print(".--.-.-.-.--..-.-")
+print("MAE mean:", np.mean(scaffold_split))
+print("Standard Deviation:", np.std(scaffold_split))
+
+print("----------------------------------")
+print("Random split")
+print("----------------------------------")
+random_split = linear_regression.lr_rando_split(df_act, df_conf, df_desc, runs)
+print(random_split)
+print(".--.-.-.-.--..-.-")
+print("MAE mean:", np.mean(random_split))
+print("Standard Deviation:", np.std(random_split))
+
+# --------------------------------------
+# Pairing results
+# --------------------------------------
+paired_dif = np.array(scaffold_split) - np.array(random_split)
+print("----------------------------------")
+print("Paired differences")
+print("----------------------------------")
+print(paired_dif)
+print(".--.-.-.-.--..-.-")
+print("Paired difference mean:", np.mean(paired_dif))
+print("Paired difference mean:", np.std(paired_dif))
